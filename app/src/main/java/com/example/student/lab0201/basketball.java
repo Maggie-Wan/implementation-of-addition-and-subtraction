50a361b6a008a56f8cde8f7144974de5f63b6541
maggie.wan <maggie.wan@aiunion.com.tw>
2019-04-25 11:39:17 +0800

    add comment

--- app/src/main/java/com/example/student/lab0201/basketball.java
+++ app/src/main/java/com/example/student/lab0201/basketball.java
@@ -21,21 +21,28 @@ public class basketball extends AppCompatActivity {
         clear = (Button) findViewById(R.id.clear);
     }
 
+    //add 3 points
     public void add3(View v) {
         mPoint+=3;
         String mP=String.valueOf(mPoint);
         score.setText(mP);
     }
+
+    //add 2 points
     public void add2(View v) {
         mPoint+=2;
         String mP=String.valueOf(mPoint);
         score.setText(mP);
     }
+
+    //freethrow add 1 point
     public void add1(View v) {
         mPoint+=1;
         String mP=String.valueOf(mPoint);
         score.setText(mP);
     }
+
+    //clean
     public void clear(View v) {
         //要先讓分數歸0
         mPoint=0;
